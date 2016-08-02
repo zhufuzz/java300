ac6c726e1bdd9112716d358e64688901a5efff85
b <b@b-520>
2016-08-02 10:12:48 -0400

    Test2 HAHAHAHA

--- src/edu/nyu/cs/test/Test2.java
+++ src/edu/nyu/cs/test/Test2.java
@@ -6,7 +6,7 @@ public class Test2 {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
 
-		System.out.println("Hello HAHAHAHA");
+		System.out.println("Hello HAHAHAHA!");
 	}
 
 }
