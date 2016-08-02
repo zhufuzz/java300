ccc62944dbc292b04bf17ffc3db714d5b201aee7
zhufuzz <zhufuzz@hotmail.com>
2016-08-02 09:20:28 -0400

    Test2 added

--- src/edu/nyu/cs/test/Test2.java
+++ src/edu/nyu/cs/test/Test2.java
@@ -1,10 +1,10 @@
 package edu.nyu.cs.test;
 
-public class Test {
+public class Test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hehe");
+
 	}
 
 }
